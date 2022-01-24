8e7ac9202fe8838ffc7b6a45925f1fce2a5bc6e5
Harsh Bhatt <98325507+Harsh7520@users.noreply.github.com>
2022-01-24 18:57:45 +0530

    Create up or down?.py

---
+++ day-16/up or down?.py
@@ -0,0 +1,10 @@
+n = int(input())
+matrix = []
+for i in range(n):
+    a = list(map(int,input().split()))
+    a.reverse()
+    for i in range(len(a)):
+        a[i] = 0 if a[i] else 1
+    matrix.append(a)
+for i in matrix:
+    print(*i)
