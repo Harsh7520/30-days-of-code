d78b57993f2ebb99284db7b05c43baa8e905a37e
Harsh Bhatt <98325507+Harsh7520@users.noreply.github.com>
2022-01-24 18:46:54 +0530

    Create Find The Younger One!.py

---
+++ day-6/Find The Younger One!.py
@@ -0,0 +1,10 @@
+n = int(input())
+a = list(map(int,input().split()))
+b = []
+for i in range(len(a)):
+    count = 0
+    for j in range(len(a)):
+       if(a[i]>a[j]):
+           count += 1
+    b.append(count)
+print(*b)
