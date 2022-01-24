de0ece4bd96da2451dde3b6ee8fcf660828c8d52
Harsh Bhatt <98325507+Harsh7520@users.noreply.github.com>
2022-01-24 18:45:52 +0530

    Create Compare and compile.py

---
+++ day-5/Compare and compile.py
@@ -0,0 +1,9 @@
+n = int(input())
+str_n = str(n)
+sqr = n*n
+str_sqr = str(sqr)
+rev = str_sqr[::-1]
+if(rev.startswith(str_n[::-1])):
+    print("Phoenix number")
+else:
+    print("Not a phoenix number.")
