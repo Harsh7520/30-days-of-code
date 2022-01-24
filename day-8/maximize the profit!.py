203f9446cc2773480c4bfe3a8b0cb4ea691884e4
Harsh Bhatt <98325507+Harsh7520@users.noreply.github.com>
2022-01-24 18:49:36 +0530

    Create maximize the profit!.py

---
+++ day-8/maximize the profit!.py
@@ -0,0 +1,10 @@
+n = int(input())
+a = list(map(int, input().split()))
+mini = min(a)
+minii = a.index(mini)
+s_a = a[minii:]
+m_s_a = max(s_a)
+if(m_s_a>=mini):
+    print(m_s_a-mini)
+else:
+    print(0)
